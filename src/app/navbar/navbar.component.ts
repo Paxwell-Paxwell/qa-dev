9b5b92ced5fc6f7664ed9e5145d9d6271a0bf9c3
Paxwell-Paxwell <pompom231245@gmail.com>
2024-04-04 09:07:22 +0700

    add css navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  public currentUrl!: string;
+  constructor(private router: Router) {
+    // Listen to routing events
+    this.router.events.subscribe((event) => {
+      // Check for the specific event type if needed, for example NavigationEnd
+      if (event instanceof NavigationEnd) {
+        // console.log(this.router.url); // Current path excluding query parameters
+        this.currentUrl = this.router.url;
+      }
+    });
+  }
 }
